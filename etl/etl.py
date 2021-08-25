a9c14bb6df46d0209149d2385904cf1a14e9b871
Michael Mallok <derschnabel@googlemail.com>
2021-08-26 00:12:24 +0200

    improved etl

--- etl/etl.py
+++ etl/etl.py
@@ -1,16 +1,41 @@
-from sqlalchemy import create_engine
 import time
+from sqlalchemy import create_engine
+import pandas as pd
+import pymongo
 
 time.sleep(5)
 
 pg = create_engine("postgresql://postgres:postgres@postgresdb:5432/tweet_db", echo=True)
-pg.execute(
+
+for currency in currencies:
+    pg.execute(
+        f"""
+        CREATE TABLE IF NOT EXISTS {currency}prices (
+        index INT,
+        close DOUBLE PRECISION,
+        time TIMESTAMPTZ,
+        sentiment NUMERIC
+    );
+    SET TIMEZONE to "EUROPE/PARIS";
     """
-    CREATE TABLE IF NOT EXISTS tweets (
-    index INT,
-    handle VARCHAR(255),
-    tweet VARCHAR(500),
-    sentiment NUMERIC
-);
-"""
-)
+    )
+    df = pd.DataFrame(columns=["close", "time"])
+
+    # Establish a connection to the MongoDB server
+    client = pymongo.MongoClient("mongodb")
+
+    # Select the database you want to use withing the MongoDB server
+    db = client.prices_data
+
+    # Select the collection of documents you want to use withing the MongoDB database
+    collection = db["prices"]
+
+    entries = collection.find()
+
+    dicti = {}
+    for e in entries:
+        dicti["close"] = e["close"]
+        dicti["time"] = e["time"]
+        df = df.append(dicti, ignore_index=True)
+
+    df.to_sql(f"{currency}prices", pg, if_exists="append")
