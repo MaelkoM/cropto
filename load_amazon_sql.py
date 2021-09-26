7422f45d55dcf45b9c77a7535868f62143ce7d0b
Michael Mallok <derschnabel@googlemail.com>
2021-09-26 12:07:00 +0200

    update sep 26

--- load_amazon_sql.py
+++ load_amazon_sql.py
@@ -2,76 +2,59 @@ import io
 import os
 import time
 
-import krakenex
-import matplotlib
 import pandas as pd
 import numpy as np
-import psycopg2
-from sqlalchemy.sql.expression import column
 import tqdm
-from numpy.lib.function_base import select
-from pykrakenapi import KrakenAPI
-from sqlalchemy import (
-    Column,
-    ForeignKey,
-    Integer,
-    MetaData,
-    String,
-    Table,
-    create_engine,
-    desc,
-    func,
-    inspect,
-    select,
-    text,
-)
-
+from sqlalchemy import create_engine
 import config
 
-user = config.DB_USER
-password = config.DB_PASSWORD
-host = config.DB_ENDPOINT
-port = "5432"
-database = config.DB_NAME
-uri = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-engine = create_engine(uri, echo=True)
-metadata = MetaData()
 
-timestamp = int(time.time() * 1000)
+class PostgresCommunicator:
+    def __init__(self) -> None:
+        self.user = config.DB_USER
+        self.password = config.DB_PASSWORD
+        self.host = config.DB_ENDPOINT
+        self.port = "5432"
+        self.database = config.DB_NAME
+        self.uri = f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
+        self.engine = create_engine(self.uri, echo=True)
+        self.timestamp = int(time.time() * 1000)
 
-for file in tqdm.tqdm(os.listdir("data/Kraken_OHLCVT")):
-    if file.endswith("csv"):
-        pair = file.split(".")[0].lower()
-        print(pair)
-        df = pd.read_csv(
-            "data/Kraken_OHLCVT/" + file,
-            sep=",",
-            names=[
-                "timestamp",
-                "open",
-                "high",
-                "low",
-                "close",
-                "volume",
-                "trades",
-                "vwap",
-            ],
-        )
-        df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
-        df["vwap"] = np.cumsum(
-            df["volume"] * (df["open"] * df["high"] * df["close"]) / 3
-        ) / np.cumsum(df["volume"])
-        print(df)
 
-        df.head(0).to_sql(
-            pair.lower(), engine, if_exists="replace", index=False
-        )  # drops old table and creates new empty table
+def load_history_tables(self) -> None:
+    for file in tqdm.tqdm(os.listdir("data/Kraken_OHLCVT")):
+        if file.endswith("csv"):
+            pair = file.split(".")[0].lower()
+            print(pair)
+            df = pd.read_csv(
+                "data/Kraken_OHLCVT/" + file,
+                sep=",",
+                names=[
+                    "timestamp",
+                    "open",
+                    "high",
+                    "low",
+                    "close",
+                    "volume",
+                    "trades",
+                    "vwap",
+                ],
+            )
+            df["timestamp"] = pd.to_datetime(df["timestamp"], unit="s")
+            df["vwap"] = np.cumsum(
+                df["volume"] * (df["open"] * df["high"] * df["close"]) / 3
+            ) / np.cumsum(df["volume"])
+            print(df)
 
-        conn = engine.raw_connection()
-        cur = conn.cursor()
-        output = io.StringIO()
-        df.to_csv(output, sep="\t", header=False, index=False)
-        output.seek(0)
-        contents = output.getvalue()
-        cur.copy_from(output, pair.lower(), null="")  # null values become ''
-        conn.commit()
+            df.head(0).to_sql(
+                pair.lower(), self.engine, if_exists="replace", index=False
+            )  # drops old table and creates new empty table
+
+            conn = self.engine.raw_connection()
+            cur = conn.cursor()
+            output = io.StringIO()
+            df.to_csv(output, sep="\t", header=False, index=False)
+            output.seek(0)
+            contents = output.getvalue()
+            cur.copy_from(output, pair.lower(), null="")  # null values become ''
+            conn.commit()
---
+++ twitter_scraper.py
@@ -0,0 +1,73 @@
+import config
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+import logging
+
+
+def authenticate():
+    """Function for handling Twitter Authentication. Please note
+    that this script assumes you have a file called config.py
+    which stores the 4 required authentication tokens:
+
+    1. API_KEY
+    2. API_SECRET
+    3. ACCESS_TOKEN
+    4. ACCESS_TOKEN_SECRET
+    """
+    auth = OAuthHandler(config.TWITTER_API_KEY, config.TWITTER_API_SECRET)
+    auth.set_access_token(
+        config.TWITTER_ACCESS_TOKEN, config.TWITTER_ACCESS_TOKEN_SECRET
+    )
+
+    return auth
+
+
+class MaxTweetsListener(StreamListener):
+    def __init__(self, max_tweets, *args, **kwargs):
+        # initialize the StreamListener
+        super().__init__(*args, **kwargs)
+        # set the instance attributes
+        self.max_tweets = max_tweets
+        self.counter = 0
+        self.tl = tl()
+        logging.basicConfig(format="%(asctime)s %(message)s", filename="streaming.log")
+
+    def on_connect(self):
+        logging.info("connected. listening for incoming tweets")
+        print("connected. listening for incoming tweets")
+
+    def on_status(self, status):
+        """Whatever we put in this method defines what is done with
+        every single tweet as it is intercepted in real-time"""
+
+        # increase the counter
+        self.counter += 1
+
+        tweet = {
+            "text": status.text,
+            "username": status.user.screen_name,
+            "followers_count": status.user.followers_count,
+        }
+        tl.store_tweet(tweet)
+        logging.info(f'New tweet arrived: {tweet["text"]}')
+        print(f'New tweet arrived: {tweet["text"]}')
+
+        # check if we have enough tweets collected
+        if self.max_tweets == self.counter:
+            # reset the counter
+            self.counter = 0
+            # return False to stop the listener
+            return False
+
+    def on_error(self, status):
+        if status == 420:
+            logging.warning("Rate limit applies. Stop the stream.")
+            print(f"Rate limit applies. Stop the stream.")
+            return False
+
+
+def tweet_listener(max_tweets):
+    auth = authenticate()
+    listener = MaxTweetsListener(max_tweets)
+    stream = Stream(auth, listener)
+    stream.filter(track=["berlin"], languages=["en"], is_async=False)
